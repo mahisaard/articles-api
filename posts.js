e48e98c6aa83ccb6fe2c28c0f9446c418139de19
Mahisa Ardana Wijaya <mahisaardanaw@gmail.com>
2022-12-06 14:49:45 +0700

    initial commit

---
+++ posts.js
@@ -0,0 +1,104 @@
+module.exports = [
+    {
+      "_id": "638eeb724883eaad8e4ab592",
+      "picture": "https://hantu.bo.satupersen.net/content/images/2022/04/employee-gb0a09c4e0_640.png",
+      "authorName": "Cris Rearisky",
+      "title": "Kesehatan Mental Menurun, Harus Apa?",
+      "releaseDate": "2014-01-28T05:54:15 -07:00",
+      "description": "Sebagai manusia, pasti ada saat-saat di mana kita merasa sedih atau tertekan. Terkadang hal itu sampai mengganggu aktivitas kita sehari-hari. Kita kehilangan nafsu makan, minat untuk melakukan hobi, punya kekhawatiran terhadap banyak hal, sampai kesulitan untuk tidur dan beraktivitas. Sebenarnya, itu kenapa sih? Apakah ada yang salah dengan kesehatan mental kita? Kenapa, dan apa yang harus dilakukan? Kalau kamu sedang mengalami situasi tersebut dan bingung harus ngapain, kamu berada di artikel yang tepat.\r\n",
+      "tags": [
+        "est",
+        "ad",
+        "fugiat",
+        "ea",
+        "est",
+        "nostrud",
+        "ut"
+      ]
+    },
+    {
+      "_id": "638eeb72286b1bf5f60b6f1a",
+      "picture": "https://hantu.bo.satupersen.net/content/images/size/w1000/2022/11/image.png",
+      "authorName": "Product Satu Persen",
+      "title": "Cara Berempati Ala Psikolog\r\n",
+      "releaseDate": "2021-07-05T12:21:02 -07:00",
+      "description": "Susah relate? Sulit buat paham perasaan yang lagi dia rasain? Atau mungkin bingung sama jalan pikiran dia? Kalau lo pernah atau bahkan sering merasakan hal ini, baca artikelnya sampai habis ya! Permasalahan yang lo hadapi di atas menunjukkan bahwa lo perlu belajar mengenai empati. Empati itu apa sih? Empati itu adalah suatu kemampuan untuk memahami posisi orang lain serta menghayati pengalaman orang lain dari sisi emosional maupun kognitif..\r\n",
+      "tags": [
+        "culpa",
+        "duis",
+        "aliquip",
+        "cillum",
+        "velit",
+        "do",
+        "pariatur"
+      ]
+    },
+    {
+      "_id": "638eeb72d227af7f63cd596b",
+      "picture": "https://hantu.bo.satupersen.net/content/images/2022/04/woman-g6636fbe9d_640.jpg",
+      "authorName": "Cris Rearisky",
+      "title": "7 Tanda Kesehatan Mentalmu dalam Keadaan Baik.\r\n",
+      "releaseDate": "2022-06-13T09:53:42 -07:00",
+      "description": "Coba bayangkan kalau kamu sehat secara fisik–nggak punya penyakit fisik, juga bisa bergerak dengan bebas dan aktif. Tapi di sisi lain, kamu nggak bahagia, nggak punya motivasi untuk melakukan apapun, susah berkonsentrasi, atau bahkan punya pikiran kalau hidup itu nggak berguna. Walaupun secara fisik kamu sehat, tapi keadaan mentalmu yang nggak sehat membuatmu nggak bisa produktif dan menikmati hidup dengan seutuhnya. Meski begitu, stress dan tekanan adalah hal wajar yang pernah dialami oleh semua orang. Sebagai manusia, kita pasti punya ups-and-downs yang beragam. Ada hari-hari yang berjalan dengan baik, ada juga hari-hari yang berjalan dengan buruk. Mereka semua pernah hadir dan pergi dalam hidup kita. Tanpa disadari, mungkin kita justru mengalami perubahan positif sebagai akibat dari pasang surut kehidupan. Bisa jadi, ternyata selama ini segala usaha yang kita lakukan untuk menjaga kesehatan mental kita membuahkan hasil yang positif.\r\n",
+      "tags": [
+        "sunt",
+        "eiusmod",
+        "et",
+        "ut",
+        "nostrud",
+        "exercitation",
+        "adipisicing"
+      ]
+    },
+    {
+      "_id": "638eeb72b1c7dba6c25d09d4",
+      "picture": "https://hantu.bo.satupersen.net/content/images/size/w1000/2022/03/Request-Thumbnail-Blog-Dampak-Menahan-Amarah-01.jpg",
+      "authorName": "Fathur Rachman",
+      "title": "5 Dampak Buruk Menahan Amarah, Kenapa Nggak Boleh Menahan Amarah?.\r\n",
+      "releaseDate": "2017-01-19T07:59:07 -07:00",
+      "description": "Apakah kalian pernah mengalami hari yang buruk, tapi tetap memperlihatkan raut bahagia ke orang sekitar? Bagi sebagian orang, hal ini dilakukan agar mereka terlihat kuat di mata orang lain. Padahal, segala emosi negatif akan berdampak buruk kalau ditahan dalam diri seseorang. Emosi negatif bisa mencakup banyak hal, mulai dari rasa frustasi, marah, hingga kekecewaan. Namun, ketika emosi negatif itu lepas dari dalam diri kita, seringkali kita disalahkan atas perilaku tersebut. Misalnya ketika sedang bersedih, biasanya orang di sekitar menyuruh untuk diam dan menghapuskan air mata kita. Begitu juga rasa marah yang sering dilihat buruknya saja. Seakan-akan, kita harus selalu berbuat baik di depan seseorang setiap saat. Hal inilah yang membuat seseorang takut untuk bercerita tentang perasaannya. Sehingga, akhirnya lebih memilih untuk menahan rasa marahnya agar ngga menyinggung orang lain..\r\n",
+      "tags": [
+        "sint",
+        "commodo",
+        "anim",
+        "reprehenderit",
+        "aute",
+        "voluptate",
+        "ut"
+      ]
+    },
+    {
+      "_id": "638eeb726d5e3c007f8d50c7",
+      "picture": "https://hantu.bo.satupersen.net/content/images/size/w1000/2022/03/Request-Thumbnail-Blog-Emotional-Sponge-01.jpg",
+      "authorName": "Restu Lestari Wulan Utami",
+      "title": "Mengenal Emotional Sponge, Kepribadian yang Mudah Menyerap Stres.\r\n",
+      "releaseDate": "2017-06-22T05:33:27 -07:00",
+      "description": "Halo, Perseners! Ketemu lagi sama aku, Restu, Part-time Blog Writer di Satu Persen. Semoga kamu sedang dalam kondisi sehat, perasaan tenang, dan mengelola emosi dengan baik di mana pun kamu berada. Bicara tentang emosi, apakah kamu memiliki empati berlebih pada orang yang mengalami masalah sehingga kamu menyerap emosi negatif pada orang itu? Pernah ngga kamu mudah merasa  stres setelah mendengar kisah memilukan yang sedang dialami oleh orang terdekatmu?.\r\n",
+      "tags": [
+        "excepteur",
+        "ipsum",
+        "laborum",
+        "tempor",
+        "est",
+        "incididunt",
+        "incididunt"
+      ]
+    },
+    {
+      "_id": "638eeb724563eeb55129bbae",
+      "picture": "http://placehold.it/32x32",
+      "authorName": "Arnold Byers",
+      "title": "Reprehenderit consectetur culpa nostrud esse enim laborum aliqua. Anim ullamco fugiat Lorem aliqua quis labore sit quis reprehenderit do. In do fugiat eu ut eu duis dolor. Et est exercitation nisi ad ex. Nostrud ex aute eu veniam est quis occaecat reprehenderit.\r\n",
+      "releaseDate": "2015-03-06T09:15:10 -07:00",
+      "description": "Cillum fugiat eiusmod ipsum laboris excepteur laboris Lorem proident et reprehenderit ipsum reprehenderit. Consectetur dolor eu laboris voluptate ex consectetur sint qui dolore voluptate minim labore tempor. Incididunt ad in sint nostrud voluptate proident irure. Dolore consectetur mollit occaecat veniam exercitation aute excepteur tempor ea ad ipsum enim incididunt quis. Non dolor ipsum ullamco ad.\r\nUllamco dolore id irure est sunt sit duis labore ullamco eiusmod ad ad esse sint. Laborum ut consequat commodo duis in occaecat in veniam. Dolor Lorem elit sit nisi in ex commodo voluptate esse nostrud amet esse ea mollit. Sit occaecat amet eu occaecat dolor minim fugiat quis laborum sunt cupidatat.\r\n",
+      "tags": [
+        "aliquip",
+        "culpa",
+        "nulla",
+        "sit",
+        "dolor",
+        "dolor",
+        "officia"
+      ]
+    }
+  ]
